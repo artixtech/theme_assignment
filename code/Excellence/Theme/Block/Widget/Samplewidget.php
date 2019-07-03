0f5f391a882d28f59d7306b539dc550e766cfdb4
singharti <artisingh3106@gmail.com>
2019-07-03 18:58:51 +0530

    Formatted Code

--- code/Excellence/Theme/Block/Widget/Samplewidget.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Excellence\Theme\Block\Widget;
-use Magento\Framework\View\Element\Template;
-use Magento\Widget\Block\BlockInterface;
-class Samplewidget extends Template implements BlockInterface
-{
-    protected $_template = "widget/samplewidget.phtml";
-}
